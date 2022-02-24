af396e75293b1cf1e6e5ba26fa99d86e97cf9424
Chris Wu <chris@faros.ai>
2022-02-24 11:07:53 -0800

    Disable airbyte init

--- init/scripts/entrypoint.sh
+++ init/scripts/entrypoint.sh
@@ -9,7 +9,7 @@ db_host=$FAROS_DB_HOST
 db_port=$FAROS_DB_PORT
 
 ./wait-for.sh $db_host:$db_port -- ./db-init.sh
-./wait-for.sh $airbyte_url/api/v1/health -t 60 -- node ../lib/airbyte/init $airbyte_url $faros_dest_version $airbyte_force_setup
+#./wait-for.sh $airbyte_url/api/v1/health -t 60 -- node ../lib/airbyte/init $airbyte_url $faros_dest_version $airbyte_force_setup
 ./wait-for.sh $hasura_url/healthz -t 60 -- node ../lib/hasura/init $hasura_url
 ./wait-for.sh $metabase_url/api/health -t 60 -- ./metabase-init.sh
 
